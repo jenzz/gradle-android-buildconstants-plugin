6cf7217147f6a555354861df03e9e1421453fdc4
Jens Driller <jens@jensdriller.com>
2016-05-02 16:59:57 +0100

    Use generated constants in sample project

--- sample/src/main/java/com/jenzz/buildconstants/sample/MainActivity.java
+++ sample/src/main/java/com/jenzz/buildconstants/sample/MainActivity.java
@@ -1,7 +1,26 @@
 package com.jenzz.buildconstants.sample;
 
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+  @SuppressWarnings("unused")
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Java constants
+    String javaString = SampleBuildConstants.ASTRING;
+    boolean javaBoolean = SampleBuildConstants.ABOOLEAN;
+    int javaNumber = SampleBuildConstants.ANUMBER;
+
+    // XML constants
+    Resources res = getResources();
+    String xmlString = res.getString(R.string.astring);
+    boolean xmlBoolean = res.getBoolean(R.bool.aboolean);
+    int xmlNumber = res.getInteger(R.integer.anumber);
+  }
 }
